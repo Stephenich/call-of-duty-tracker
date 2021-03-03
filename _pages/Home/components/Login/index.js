5e2970ad239af62154e8f00b3b2f8ad994a172b9
Stephenich <sma.gurney@gmail.com>
2021-03-03 11:24:54 +0000

    added linter and formater with rules

--- _pages/Home/components/Login/index.js
+++ _pages/Home/components/Login/index.js
@@ -1,23 +1,22 @@
 import { Formik, Field, Form } from 'formik';
 
 export function LoginForm() {
-    return (
+  return (
     <div className="Form">
-        <h1>Login</h1>
-        <Formik
-            initialValues={{ email: "", password: "" }}
-            onSubmit={async values => {
-                await new Promise(resolve => setTimeout(resolve, 500));
-                alert(JSON.stringify(values, null, 2));
-            }}
-        >
-            <Form>
-                <Field name="email" type="email" />
-                <Field name="password" type="password" />
-                <button type="submit">Submit</button>
-            </Form>
-        </Formik>
+      <h1>Login</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <Field name="email" type="email" />
+          <Field name="password" type="password" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </div>
-    )
-};
-
+  );
+}
