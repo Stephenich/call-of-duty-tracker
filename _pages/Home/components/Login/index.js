02810a822e9b38e2bbf17acfef9d632f1d1d239a
Stephenich <sma.gurney@gmail.com>
2021-03-23 12:00:55 +0000

    Added login functionality to longin page, added dashboard page

--- _pages/Home/components/Login/index.js
+++ _pages/Home/components/Login/index.js
@@ -1,22 +1,41 @@
 import { Formik, Field, Form } from 'formik';
 import styles from './styles/login.module.css';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import { useRouter } from 'next/router';
+
+const cookies = new Cookies();
 
 export function LoginForm() {
+  const router = useRouter();
   return (
     <div className={styles.form}>
       <h1>Login</h1>
       <Formik
-        initialValues={{ email: '', password: '' }}
+        initialValues={{ email: 'stephen-1310@hotmail.com', password: '' }}
         onSubmit={async (values) => {
-          await new Promise((resolve) => setTimeout(resolve, 500));
-          alert(JSON.stringify(values, null, 2));
+          // await new Promise((resolve) => setTimeout(resolve, 500));
+          // alert(JSON.stringify(values, null, 2));
+          const {
+            data: { token },
+          } = await axios.post('/api/login', {
+            username: values.email,
+            password: values.password,
+          });
+          cookies.set('token', token);
+          console.log(token);
+          router.push('/dashboard');
         }}
       >
-        <Form>
-          <Field name="email" type="email" />
-          <Field name="password" type="password" />
-          <button type="submit">Submit</button>
-        </Form>
+        {({ isSubmitting }) => (
+          <Form>
+            <Field name="email" type="email" />
+            <Field name="password" type="password" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
