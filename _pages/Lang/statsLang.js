9150cdfb538b0c6f505738c89e53b34020cfd194
Stephenich <sma.gurney@gmail.com>
2021-04-22 13:12:53 +0100

    Added statsCard component

---
+++ _pages/Lang/statsLang.js
@@ -0,0 +1,21 @@
+const lang = {
+  cw: { kdratio: 'Kill Death Ratio', deaths: 'Deaths', wins: 'Wins', kills: 'Kills', gamesPlayed: 'Total Games' },
+  mw: {
+    wins: 'Wins',
+    kills: 'Kills',
+    downs: 'Downs',
+    deaths: 'Deaths',
+    topTwentyFive: 'Top 25',
+    scorePerMinute: 'Score per minute',
+    topTen: 'Top 10: ',
+    topFive: 'Top 5',
+    gamesPlayed: 'Total Games',
+    timePlayed: 'Total Time',
+    score: 'Score',
+    revives: 'Revives',
+    contracts: 'Contracts',
+    kdratio: 'Kill Death Ratio',
+  },
+};
+
+export default lang;
